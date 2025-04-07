cc054e26726954e7d29dc8d371cef56e214d4070
Rentoshiro <r.renat1998@gmail.com>
2025-04-08 00:14:17 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Routes, Route, Link } from "react-router-dom";
 import { Counter } from "./components/Counter";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutPage } from "./pages/AboutPage/AboutPage";
 import { MainPage } from "./pages/MainPage/MainPage";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to={"/about"}>Main</Link>
       <Link to={"/main"}>About</Link>
+      <button onClick={() => toggleTheme()}>Change theme</button>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Counter />} />
