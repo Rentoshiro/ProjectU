723a8a209390edcc0c7cb15a06d83dba51d9fd5a
Rentoshiro <r.renat1998@gmail.com>
2025-05-09 21:41:17 +0300

    upd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+// .eslintrc.js
 module.exports = {
   env: {
     browser: true,
@@ -22,23 +23,27 @@ module.exports = {
   ],
   plugins: ["@typescript-eslint", "react", "jsx-a11y", "i18next"],
   rules: {
+    // Force double quotes
     quotes: ["error", "double"],
+
+    // Windows line endings
     "linebreak-style": ["error", "windows"],
+
+    // JS/TS indent of 2 spaces
+    indent: ["error", 2],
+
+    // JSX children indent of 2 spaces
+    "react/jsx-indent": ["error", 2],
+    // JSX props indent of 2 spaces
+    "react/jsx-indent-props": ["error", 2],
+
+    // Allow .ts/.tsx files to contain JSX
     "react/jsx-filename-extension": [
       "error",
-      {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
     ],
-    "react/jsx-indent": [
-      "error",
-      2,
-      {
-        checkAttributes: true,
-        indentLogicalExpressions: true,
-        indentChildren: true,
-      },
-    ],
+
+    // i18next: no literal strings in markup, with some attributes ignored
     "i18next/no-literal-string": [
       "error",
       {
