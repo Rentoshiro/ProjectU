7c571b27e1597b73abd223a8e674128a69923217
Rentoshiro <r.renat1998@gmail.com>
2025-05-09 17:20:43 +0300

    update: add async component loading

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -4,7 +4,12 @@ import { useTranslation } from "react-i18next";
 const MainPage = () => {
   const { t } = useTranslation();
 
-  return <div>{t("Главная страница")}</div>;
+  return (
+    <div>
+      <div></div>
+      {t("Главная страница")}
+    </div>
+  );
 };
 
 export default MainPage;
