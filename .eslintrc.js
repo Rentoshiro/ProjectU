f124de05b6481c0d13d4a30a0adbf33e83fb4d83
Rentoshiro <r.renat1998@gmail.com>
2025-05-10 13:22:47 +0300

    upgrade sidebar and navbar

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,86 @@
 // .eslintrc.js
+
 module.exports = {
   env: {
     browser: true,
+
     es2021: true,
   },
+
   parser: "@typescript-eslint/parser",
+
   parserOptions: {
     ecmaVersion: "latest",
+
     sourceType: "module",
+
     ecmaFeatures: { jsx: true },
   },
+
   settings: {
     react: { version: "detect" },
   },
+
   extends: [
     "airbnb",
+
     "plugin:@typescript-eslint/recommended",
+
     "plugin:react/recommended",
+
     "plugin:jsx-a11y/recommended",
+
     "plugin:i18next/recommended",
+
     "plugin:storybook/recommended",
   ],
+
   plugins: ["@typescript-eslint", "react", "jsx-a11y", "i18next"],
+
   rules: {
     // Force double quotes
+
     quotes: ["error", "double"],
 
     // Windows line endings
+
     "linebreak-style": ["error", "windows"],
 
     // JS/TS indent of 2 spaces
+
     indent: ["error", 2],
 
     // JSX children indent of 2 spaces
+
     "react/jsx-indent": ["error", 2],
+
     // JSX props indent of 2 spaces
+
     "react/jsx-indent-props": ["error", 2],
 
     // Allow .ts/.tsx files to contain JSX
+
     "react/jsx-filename-extension": [
       "error",
+
       { extensions: [".js", ".jsx", ".ts", ".tsx"] },
     ],
 
     // i18next: no literal strings in markup, with some attributes ignored
+
     "i18next/no-literal-string": [
       "error",
+
       {
         markupOnly: true,
+
         ignoreAttribute: ["data-testid", "to", "role", "className", "id"],
       },
     ],
   },
+
   globals: {
     __IS_DEV__: true,
   },
 };
+
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,9 +2,14 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { useState } from "react";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
-import { Button } from "shared/ui/Button/Button";
+import { Button, ThemeButton } from "shared/ui/Button/Button";
 import { useTranslation } from "react-i18next";
 import cls from "./Sidebar.module.scss";
+import { ButtonSize } from "shared/ui/Button/Button";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { RoutePath } from "shared/config/routeConfig/routeConfig";
+import AboutIcon from "shared/assets/icons/about-20-20.svg";
+import MainIcon from "shared/assets/icons/main-20-20.svg";
 
 interface SidebarProps {
   className?: string;
@@ -25,13 +30,43 @@ export const Sidebar = ({ className }: SidebarProps) => {
         className,
       ])}
     >
-      <Button data-testid="sidebar-toggle" onClick={onToggle}>
-        {t("Переключить")}
+      <Button
+        className={cls.collapseBtn}
+        data-testid="sidebar-toggle"
+        onClick={onToggle}
+        theme={ThemeButton.BACKGROUND_INVERTED}
+        square
+        size={ButtonSize.L}
+      >
+        {collapsed ? ">" : "<"}
       </Button>
+      <div className={cls.items}>
+        <div>
+          <AppLink
+            className={cls.item}
+            theme={AppLinkTheme.SECONDARY}
+            to={RoutePath.main}
+          >
+            <MainIcon className={cls.icon}></MainIcon>
+            <span className={cls.link}>{t("Главная")}</span>
+          </AppLink>
+        </div>
+        <div>
+          <AppLink
+            className={cls.item}
+            theme={AppLinkTheme.SECONDARY}
+            to={RoutePath.about}
+          >
+            <AboutIcon className={cls.icon}></AboutIcon>
+            <span className={cls.link}>{t("О сайте")}</span>
+          </AppLink>
+        </div>
+      </div>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        <LangSwitcher className={cls.lang} />
+        <LangSwitcher className={cls.lang} short={collapsed} />
       </div>
     </div>
   );
 };
+
